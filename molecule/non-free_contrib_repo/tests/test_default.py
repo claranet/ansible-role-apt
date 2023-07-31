32aec27a40fa724060c304f88ab3396f05eb88a6
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-07-31 15:17:02 +0200

    tests: check apache2 package install

--- molecule/non-free_contrib_repo/tests/test_default.py
+++ molecule/non-free_contrib_repo/tests/test_default.py
@@ -6,11 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_album(host):
-    p = host.package('album')
-    assert p.is_installed
-
-
-def test_java_package(host):
-    p = host.package('java-package')
+def test_package(host):
+    p = host.package('apache2')
     assert p.is_installed
